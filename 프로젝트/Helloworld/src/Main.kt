6ed831f67ed4faf65b722bb61edf5ce47a549766
nayeon <nayeonvita@naver.com>
2020-10-10 21:10:53 +0900

    [add] kotlin hello
    kotlin 기초

---
+++ 프로젝트/Helloworld/src/Main.kt
@@ -0,0 +1,13 @@
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello, World!")
+            val name:String="내 이름은 Kotlin"
+            var count:Int=0
+            var rating:Double=9.14e3
+            println(name+'\n'+count+'\n'+rating)
+        }
+    }
+}
+
