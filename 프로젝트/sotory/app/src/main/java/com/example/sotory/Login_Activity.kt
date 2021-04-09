8f88b4db57307938523fcfd79d0dd56f0b8576be
nayeon <nayeonvita@naver.com>
2021-04-09 21:37:15 +0900

    [Add] sotory
    sotory

---
+++ 프로젝트/sotory/app/src/main/java/com/example/sotory/Login_Activity.kt
@@ -0,0 +1,15 @@
+package com.example.sotory
+
+import android.R
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+
+
+class Login_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
